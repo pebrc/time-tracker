7fee4d9bbb3f6ee067b29811a907b65625bd1117
Peter Brachwitz <peter.brachwitz@gmail.com>
2016-06-04 22:00:07 +0200

    added selenium and java-time

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha4"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha4"]
+                 [clj-webdriver "0.7.2"]
+                 [org.seleniumhq.selenium/selenium-java "2.52.0"]
+                 [clojure.java-time "0.2.0"]]
   :main ^:skip-aot time-tracker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
