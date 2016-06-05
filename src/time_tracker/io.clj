362d627c310669f993486399dfd8a35182996737
Peter Brachwitz <peter.brachwitz@gmail.com>
2016-06-05 14:18:09 +0200

    added io fns for edn data

---
+++ src/time_tracker/io.clj
@@ -0,0 +1,12 @@
+(ns time-tracker.io
+  (require [clojure.edn :as edn]
+           [clojure.java.io :as io]))
+
+(defn read-edn [filename]
+  (edn/read-string (slurp filename)))
+
+(defn write-edn [data filename]
+  (let [file (io/file filename)]
+    (when-not (.exists file)
+      (io/make-parents file))
+    (spit file (with-out-str (pr data)))))
