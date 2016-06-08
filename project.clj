2ae902b3f375a2905eca54ac7418e7b725b101ce
Peter Brachwitz <peter.brachwitz@gmail.com>
2016-06-08 07:30:36 +0200

    added logging. todo: config

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
                  [clj-webdriver "0.7.2"]
                  [org.seleniumhq.selenium/selenium-java "2.52.0"]
                  [clojure.java-time "0.2.0"]
-                 [org.threeten/threeten-extra "0.9"]]
+                 [org.threeten/threeten-extra "0.9"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.7"]]
   :main ^:skip-aot time-tracker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
