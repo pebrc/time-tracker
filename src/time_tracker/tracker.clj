19736dbb2021f273382d2aaaf74fc82559454cb1
pbrachwi <pbrachwi@cisco.com>
2016-06-10 18:50:11 +0200

    validate collected data

--- src/time_tracker/tracker.clj
+++ src/time_tracker/tracker.clj
@@ -1,6 +1,7 @@
 (ns time-tracker.tracker
   (require [time-tracker.store :as s]
            [time-tracker.nettime :as nettime]
+           [clojure.spec :as spec]
            [java-time :as t]))
 
 (defmulti time-data (fn [env cfg params] env))
@@ -12,10 +13,16 @@
   {:interval (t/interval (t/zoned-date-time 1970 1) (t/minus (t/zoned-date-time) (t/days 1)))
    })
 
+(defn validate [data]
+  (if-let [errors (spec/explain-data ::s/store data)]
+    (throw (ex-info "Invalid data collected" errors))
+    data))
+
 (defn track [conf]
   (let [stored  (s/read-store conf)]
     (->> (to-params stored)
          (time-data (System/getProperty "os.name") conf )
+         (validate )
          (nettime/track conf);;this could be pluggable as well
          (s/merge-stores stored)
          (s/write-store conf))))
