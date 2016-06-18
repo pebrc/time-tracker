ccd515ddd8daa689e870d46534715111f341ac8f
pbrachwi <pbrachwi@cisco.com>
2016-06-18 12:50:54 +0200

    added tz as opt config param

--- src/time_tracker/config.clj
+++ src/time_tracker/config.clj
@@ -1,6 +1,7 @@
 (ns time-tracker.config
   (require [time-tracker.io :as io]
-           [clojure.spec :as s]))
+           [clojure.spec :as s]
+           [java-time :as t]))
 
 ;;; SPECs
 (s/def ::user string?)
@@ -8,7 +9,8 @@
 (s/def ::data-dir string?)
 (def project-regex #"^\d{6}\.\d{2}$")
 (s/def ::project (s/and string? #(re-matches project-regex %)) )
-(s/def ::config (s/keys :req-un [::user ::pw ::project]))
+(s/def ::config (s/keys :req-un [::user ::pw ::project]
+                        :opt-un [:trime-tracker.store/tz]))
 
 
 
