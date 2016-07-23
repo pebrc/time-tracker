10f0ea5e1b66b49cce5b685903a96a2dfdb6aca4
Peter Brachwitz <peter.brachwitz@gmail.com>
2016-07-23 22:13:09 +0200

    upgrade to alpha10

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                  [org.clojure/tools.cli "0.3.5"]
                  [clj-webdriver "0.7.2"]
                  [com.codeborne/phantomjsdriver "1.3.0"]
